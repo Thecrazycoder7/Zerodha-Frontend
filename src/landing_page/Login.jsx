394a1aa9dcf23a657c073fb155e54511076bf8b6
Suman Kumari <86953085+Thecrazycoder7@users.noreply.github.com>
2025-07-22 12:38:10 +0530

    Update Login.jsx

--- src/landing_page/Login.jsx
+++ src/landing_page/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3000/login",
+        "https://zerodha-backend-lovat.vercel.app/login",
         inputValue,
         { withCredentials: true }
       );
